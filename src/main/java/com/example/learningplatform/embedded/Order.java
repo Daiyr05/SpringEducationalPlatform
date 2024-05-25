ad40b54c3aa0ea6eae8886c348d0c4ad7837fa5c
Daiyr05 <87783710165q@gmail.com>
2024-05-25 10:20:18 +0500

    added embeddable entity and id

---
+++ src/main/java/com/example/learningplatform/embedded/Order.java
@@ -0,0 +1,23 @@
+package com.example.learningplatform.embedded;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_order")
+public class Order {
+    @EmbeddedId
+    private OrderId id;
+    private String orderInfo;
+    private String anotherInfo;
+    @Embedded
+    private Address address;
+}
